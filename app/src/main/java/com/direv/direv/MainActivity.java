667b53a02158dd1a73216e40637da1a064cccdcc
mlandavazo <mlandavazo19@gmail.com>
2017-10-15 22:44:26 -0700

    added login and signup pages

--- app/src/main/java/com/direv/direv/MainActivity.java
+++ app/src/main/java/com/direv/direv/MainActivity.java
@@ -1,15 +1,13 @@
 package com.direv.direv;
 
 import android.app.ProgressDialog;
-import android.support.annotation.NonNull;
-import android.support.v4.text.TextUtilsCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
