0ce682a5be005ff435a1b05c4a5c04fc38b32c42
khinthein <kthein@ucmerced.edu>
2017-11-18 20:10:52 -0800

    login/regis

---
+++ app/src/main/java/com/direv/direv/Constants.java
@@ -0,0 +1,13 @@
+package com.direv.direv;
+
+/**
+ * Created by khinthein on 10/12/17.
+ */
+
+public class Constants {
+    private static final String ROOT_URL = "http://10.0.0.81/Direv/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+}
